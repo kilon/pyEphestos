bd0fba4e51fb84c6c2d1b4145a6cf137736d5315
Peter Gragert <pkhgragert@gmail.com>
2012-04-23 12:24:29 +0200

    new for splleted up morpheas

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@
 #    written by Kilon Alios
 #    thekilons@yahoo.co.uk
 #
-#    version April-2012
+#    version March-2012
 #
 #    Copyright (C) 2012 by Kilon Alios
 #---------------------------------------
@@ -69,175 +69,26 @@ bl_info = {
     "category": "Development"}
 
 
+
 if "bpy" in locals():
     import imp
-    imp.reload(Ephestos.morpheas)
-    
+    print("\n++++++if part  bpy seen in __init__ of Ephestos")
+    if "morpheas" in locals():
+        imp.reload(morpheas) 
+    if "test_1_morpheas" in locals():
+        imp.reload(test_1_morpheas)
 else:
-    from Ephestos import morpheas
-   
-
+    import Ephestos
+    print(type(Ephestos))
+    print(dir(Ephestos))
+    print("\n----else part------n in __init__ of Ephestos")
+    from Ephestos import  test_1_morpheas #, basics_morpheas,  morph_class
+#    print(dir(morpheas))
+    
 import bpy
-import bgl
-import blf
-from bpy.props import *
-pwBot = morpheas.Point(30,30)
-pwTop = morpheas.Point(200,400)
-good_rounded_box = morpheas.RoundedBox( )
-#good_rounded_box.bounds = morpheas.Point(30,30).corner(morpheas.Point(200,400))
-good_rounded_box.set_position(morpheas.Point(40,40))
-good_rounded_box.name = "roundedBox"
-good_rounded_box.color = (1, 0, 0, 1)
-good_rounded_box.alpha = 0.8
-
-text = morpheas.Text("PKHG = Peter\nline 2\nand this too and more and more")
-text.set_position(morpheas.Point(70,70))
-p1 = morpheas.Point(40,50)
-p2 = morpheas.Point(80,120)
-bounds_red = p1.corner(p2)
-
-red_morph= morpheas.Morph( bounds = bounds_red, rounded = True, with_name = True)
-green_morph= morpheas.Morph()
-blue_morph= morpheas.Morph()
-world= morpheas.World()
-
-rounded_box = morpheas.RoundedBox(border = 30)#does not work yet why???b bottom_left= p1, top_right = p2)
-rounded_box.bounds = morpheas.Point(0,0).corner(morpheas.Point(200,400))
-rounded_box.color = (1, 1, 1, .5)
-rounded_box.bordercolor = (0, 0, 0, 1)
-
-rounded_box.set_position(morpheas.Point(200,200))
-
-world.add(red_morph)
-world.add(green_morph)
-world.add(blue_morph)
-world.add(text)
-world.add(rounded_box)
-world.add(good_rounded_box)
-
-green_morph.set_position(morpheas.Point(150,150))
-blue_morph.set_position(morpheas.Point(350,350))
-hand = morpheas.Hand()
-hand.attach_to_world(world)
-red_morph.color= (1.0,0.0,0.0, 1.0)
-red_morph.name = "RED"
-green_morph.color= (0.0,1.0,0.0, 0.5)
-green_morph.name = "GREEN"
-blue_morph.color= (0.0,0.0,1.0, 0.3)
-blue_morph.name = "BLUE"
-
-class World:
-    running = False
-    mouse_region_x = 0
-    mouse_region_y = 0
-
-def draw_World(self,context):
-    global world
-    mW = world #morpheas.World() #was Morph
-    bgl.glEnable(bgl.GL_BLEND)
-#for world    morpheas.draw_rounded_morph(mW)
-    mW.draw_new()    
-    return
-
-show_world = True 
-
-
-class ephestos:
-    running = False
-    
-
-def draw_ephestos(self,context):
-    global show_world
-    bgl.glEnable(bgl.GL_BLEND)
-    bgl.glColor4f(0.0, 0.0, 0.0, 0.5)
-    bgl.glLineWidth(1.5)
-
-    """
-    #set colour to use
-    bgl.glColor4f(0.5,0.0,0.5,0.3)
-
-    x_region = round((bpy.context.area.regions[4].width-5)/2)
-    y_region = round((bpy.context.area.regions[4].height-5)/2)
-    print("x_region : ",x_region)
-    print("y_region : ",y_region)
-    bgl.glRecti(5,5,x_region, y_region)
-    """
-#    if show_world:
-    good_rounded_box.draw_new(ephestos)
-    world.draw_new(ephestos)
-#        show_world = False
-    red_morph.draw_new(ephestos)
-    green_morph.draw_new(ephestos)
-    blue_morph.draw_new(ephestos)
-    text.draw_new(ephestos)
-    rounded_box.draw_new(ephestos)
-    # restore opengl defaults
-    bgl.glLineWidth(1)
-    bgl.glDisable(bgl.GL_BLEND)
-    bgl.glColor4f(0.0, 0.0, 0.0, 1.0)
-    
-class open_ephestos(bpy.types.Operator):
-    bl_idname = "ephestos_button.modal"
-    bl_label = "Ephestos"
-
-    def modal(self, context, event):
-        result =  {'PASS_THROUGH'}
-        context.area.tag_redraw()                
-        if context.area.type == 'VIEW_3D' and ephestos.running and event.type in ('ESC'):
-            context.region.callback_remove(self._handle)
-            ephestos.running = False
-            print("CANCELLED")
-            result = {'CANCELLED'}
-        elif context.area.type == 'VIEW_3D' and ephestos.running \
-                 and event.type in ('MOUSEMOVE','LEFTMOUSE','RIGHTMOUSE')\
-                 and event.mouse_region_x > 0 \
-                 and event.mouse_region_x < bpy.context.area.regions[4].width\
-                 and event.mouse_region_y > 0 \
-                 and event.mouse_region_y < bpy.context.area.regions[4].height :
-#            print("RUNNING_MODAL")
-#            print("event type :" ,event.type)
-#            print("event value : ",event.value)
-            hand.bounds.origin = morpheas.Point(event.mouse_region_x, event.mouse_region_y)
-            hand.process_mouse_event(event) #{'RUNNING_MODAL'}            
-        else:
-#            print("event type :" ,event.type)
-#            print("event value : ",event.value)
-#            print("PASS THROUGH")
-            pass
-        return result
-
-    def invoke(self, context, event):
-        if context.area.type == 'VIEW_3D' and ephestos.running == False :
-            
-            self.cursor_on_handle = 'None'
-            context.window_manager.modal_handler_add(self)
-
-            # Add the region OpenGL drawing callback
-            # draw in view space with 'POST_VIEW' and 'PRE_VIEW'
-            self._handle = context.region.callback_add(draw_ephestos, (self, context), 'POST_PIXEL')
-#PKHG.notneeded            self._handle_world = context.region.callback_add(draw_World, (self, context), 'POST_PIXEL')            
-            ephestos.running = True
-            return {'RUNNING_MODAL'}
-        else:
-            self.report({'WARNING'}, "Ephestos is already opened and running")
-            return {'CANCELLED'}
-
-# this the main panel
-class ephestos_panel(bpy.types.Panel):
-    bl_label = "Ephestos"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS"
+#from Ephestos import morpheas , test_1_morpheas
    
-    def draw(self, context):
-        layout = self.layout
-        box = layout.box()
-        box.label(text="Ephestos WIP not finished yet")
-        box.operator("ephestos_button.modal")
-        
 
-
-
-        
 def register():
     bpy.utils.register_module(__name__)
 
