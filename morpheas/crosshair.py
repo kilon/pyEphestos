1c19a00b3c53972176ed126d903d2ecf10247f93
Peter Gragert <pkhgragert@gmail.com>
2012-06-04 08:21:06 +0200

    clean up working now.

--- morpheas/crosshair.py
+++ morpheas/crosshair.py
@@ -1,5 +1,4 @@
 #from .morph import *
-debug_print_cross = True
 from .hand import *
 class CrossHair(Morph):
     """ a slave to (a/THE) hand, but painting a Cross, dependent on is_visible"""
@@ -11,20 +10,11 @@ class CrossHair(Morph):
         self.is_visible = True
         
     def draw(self):
-        if self.is_visible:            
-            mp_x = self.hand.mouse_x
-            mp_y = self.hand.mouse_y
-            top_x = mp_x 
-            bot_y = mp_x - 10
-            top_y = mp_y + 10
-            left_x = mp_y - 10
-            right_x = mp_y + 10
-            if debug_print_cross:
-                print("  a cross must be painted at", (mp_x, mp_y))
-            #self.draw_line(mp_x, top_y, mp_x, bot_y)
-            #self.draw_line(left_x, mp_x, right_x, mp_x)
-            self.draw_line(self.hand.mouse_x -10 , self.hand.mouse_y , self.hand.mouse_x +10 , self.hand.mouse_y)
-            self.draw_line(self.hand.mouse_x , self.hand.mouse_y -10 , self.hand.mouse_x , self.hand.mouse_y +10)
+        if self.is_visible:
+            self.draw_line(self.hand.mouse_x - 10, self.hand.mouse_y,\
+                           self.hand.mouse_x + 10, self.hand.mouse_y)
+            self.draw_line(self.hand.mouse_x, self.hand.mouse_y - 10,\
+                           self.hand.mouse_x, self.hand.mouse_y + 10)
         else:
             if debug_print_cross:
                 print(" visibility of CrossHair is False")
