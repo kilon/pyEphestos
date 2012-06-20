1264b4f02cfb7b3786f157003dcd1af61b508d02
kilon <thekilon@yahoo.co.uk>
2012-06-20 14:39:10 +0300

    remove a glbegin

--- morpheas/stringfield.py
+++ morpheas/stringfield.py
@@ -15,7 +15,7 @@ re_CAS = re.compile("^(LEFT|RIGHT)_CTRL$|^(LEFT|RIGHT)_ALT$|^(LEFT|RIGHT)_SHIFT$
 re_RL_ARROW = re.compile(" \*(LEFT|RIGHT)_ARROW\*  *$")
 class OneLineText(Morph):
     """I am a single line to input text, NEEDS an owner with kbd_listener"""
-                      
+
     def __init__(self, owner = None,
                  text = "",
 #                 fontname="verdana.ttf",
@@ -36,28 +36,28 @@ class OneLineText(Morph):
         self.is_editable = False
         self.width = 10
 #        super(OneLineText, self).__init__()
-        self.color = (1, 1, 1, 1) 
+        self.color = (1, 1, 1, 1)
         tmp = addon_utils.paths()[0] + "/Ephestos/fonts/verdana.ttf"# + fontname
         self.font = blf.load(tmp)
-#PKHG.TODO DPI = ???        
+#PKHG.TODO DPI = ???
         blf.size(self.font, self.fontsize, 72) #DPI default 72?
         self.kbd_listener = self.owner.kbd_listener
         self.list_of_char_x_values =[]
         self.nr_of_chars = 0
-    
+
     def __repr__(self):
         return 'OneLineText("' + self.text + '")'
 
     def draw(self):
         """draw, if visible, my text"""
-        
+
         tmp = self.kbd_listener.text_input
         #tmp2 = re_LEFT_ARROW.search(tmp)
         res = re_RL_ARROW.search(tmp)
         if res:
             tmp2 = " *" + res.group(1) + "_ARROW* "
             tmp = tmp.replace(res.group(),"<=>")
-        self.text = tmp 
+        self.text = tmp
         self.kbd_listener.text_input = tmp
         t_width, t_height  = blf.dimensions(self.font, self.text)
         self.width = int(t_width + 2.0)
@@ -73,20 +73,20 @@ class OneLineText(Morph):
 #            new_origin = origin + Point(int(t_width),0)
             self.blinker.set_position(Point(x + int(t_width + 1), y))
             blf.draw(self.font, self.text)
-    
+
     def get_width(self):
         """ the width of the text at actual font-size"""
         return self.width
 
 #    def place_blinker(self, x, y,  dx):
 #        return Point(x + dx, y)
-        
-        
+
+
     #OneLineText menu:
 
     def add_keystroke(self):
         pass
-        
+
     '''
     def developers_menu(self):
         menu = super(OneLineText, self).developers_menu()
@@ -104,7 +104,7 @@ class OneLineText(Morph):
             menu.add_item("italic", 'set_to_italic')
         return menu
     '''
-    
+
     def edit(self):
         """change text for each kbd-release"""
         tmp = self.kbd_listener.text_input
@@ -117,7 +117,7 @@ class OneLineText(Morph):
 ###### PKHG TODO font-stuff ##############33
     '''
     def choose_font(self):
-        fontname = world.fontname_by_user() 
+        fontname = world.fontname_by_user()
         if fontname != None:
             self.fontname = fontname
             self.changed()
@@ -153,7 +153,7 @@ class OneLineText(Morph):
         self.draw()
         self.changed()
     '''
-    
+
     #OneLineText events:
 
     def handles_mouse_click(self):
@@ -172,7 +172,7 @@ class OneLineText(Morph):
 
 class StringInput( Morph):
     """StringInput is used to get/show a one-line input text-string"""
-    
+
     def __init__(self, hand, blinker,  default='I am the default',
                  minwidth=100,
                  fontname="verdana.ttf",
@@ -180,7 +180,7 @@ class StringInput( Morph):
                  bold=False,
                  italic=False):
         self.hand = hand
-        self.blinker = blinker 
+        self.blinker = blinker
         self.kbd_listener = hand.kbd_listener
         super(StringInput, self).__init__()
         self.default = default
@@ -202,12 +202,12 @@ class StringInput( Morph):
         self.activation_info.set_position(self.bounds.corner)
         self.activation_info.is_visible = False
         self.activation_info.set_color("green")
-        self.add(self.activation_info)        
+        self.add(self.activation_info)
 
     def draw(self):
         "draw and adjust size of morph, input_text dependant"
 #        super(StringInput, self).draw()
-#        self.onelinetext.draw()        
+#        self.onelinetext.draw()
         input_width = self.onelinetext.width + 5 #PKHG because of offset onelinetext
         if input_width < 100:
             dif = 0
@@ -215,7 +215,7 @@ class StringInput( Morph):
         else:
             dif = input_width - self.get_width()
 
-        if dif > 0:            
+        if dif > 0:
             new_corner = self.bounds.corner + Point(dif,0)
             self.bounds = Rectangle(self.bounds.origin, new_corner)
         elif dif < 0:
@@ -224,7 +224,7 @@ class StringInput( Morph):
             if input_width < 100:
 
                 self.bounds = Rectangle(self.bounds.origin, Point(x + 100, y))
-            else:        
+            else:
                 self.bounds = Rectangle(self.bounds.origin,Point(x + input_width, y))
         children = self.children
 #PKHG.??? next lines really needed?
@@ -232,17 +232,17 @@ class StringInput( Morph):
 #        for child in children:
 #            if child.is_visible:
 #                child.draw()
-        
+
         super(StringInput, self).draw()
         if self.activation_info.is_visible:
             self.activation_info.draw()
         self.onelinetext.draw()
 
         x = self.bounds.origin.x + 1
-        y = self.bounds.origin.y + 1        
+        y = self.bounds.origin.y + 1
         self.blinker.set_position(Point(x + input_width, y))
         self.blinker.draw()
-        return 
+        return
 
     def get_string(self):
         """ getter of input-text at THIS moment"""
@@ -256,20 +256,20 @@ class StringInput( Morph):
         """start editing of text via a mouse-click"""
         self.is_activated = not self.is_activated
 #        print("\n>>>>>> StringInput.py L210 mouse_click_left is_editable", self.is_activated)
-        
+
     def mouse_enter(self):
         self.is_activated = True
         self.onelinetext.is_editable = True
         self.kbd_listener.users += 1
         self.activation_info.is_visible = True #PKHG???False
-        
+
     def mouse_leave(self):
         self.is_activated = False
-        self.kbd_listener.users -= 1        
+        self.kbd_listener.users -= 1
         self.onelinetext.is_editable = False
         self.activation_info.is_visible = False #PKHG???False
-                
-         
+
+
     def key_release(self,event):
 #PKHG.TODO what to do with RET ... and arrow and Page-down etc???
         if True: #event.type in {'RET','NUMPAD_ENTER'}:
@@ -277,9 +277,9 @@ class StringInput( Morph):
             self.insert_committed_text(tmp)
             self.onelinetext.text = tmp
         #    set_Info_input(tmp, False)
-#PKHG.attention  return used:                        
+#PKHG.attention  return used:
         return {'RUNNING_MODAL'} #keys eaton up
-                    
+
 
     def insert_committed_text(self, text):
            print("StringInput.insert_committed_text (L241) text = ", text)
@@ -296,28 +296,27 @@ class Blinker(Morph):
         self.start_time = time()
         self.time_now = time()
         self.draw() #PKHG must be later then start_time and time_now
-        
+
     def wants_to_step(self):
         return True
 
     def step(self):
         self.time_now = time()
-        if (self.time_now - self.start_time) > self.fps :          
+        if (self.time_now - self.start_time) > self.fps :
 #            self.toggle_visibility()
             self.start_time = self.time_now
             if self.color[0] == 1.0:
                 self.set_color((0, 1.0, 0, 0.7))
             else:
                 self.set_color((1.0, 0, 0, 0.7))
-        
+
     def draw(self):
         self.step()
-        bgl.glEnable(bgl.GL_BLEND) #PKHG.??? needed?
+       # bgl.glEnable(bgl.GL_BLEND) #PKHG.??? needed?
         bgl.glColor4f(*self.color)
-        [x,y] = [self.get_position().x, self.get_position().y] 
+        [x,y] = [self.get_position().x, self.get_position().y]
 #        dimensions = self.get_extent().as_list()
         #if self.rounded:
         #    Morph.draw_rounded_morph(self, 0.3, self.color, rectangle = False)
         #else:
         bgl.glRecti(x, y, x + 3, y + 20)
-
