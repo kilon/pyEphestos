f8b44dec1626efd2bb37b933c54bd371d790b450
kilon <thekilon@yahoo.co.uk>
2012-06-28 19:48:10 +0300

    test

--- morpheas/button.py
+++ morpheas/button.py
@@ -2,10 +2,11 @@ from .menu import *
 
 class Button(RoundedBox):
     def __init__(self, target=None,
-                 action= None, 
+                 action= None,
                  label=None,
                  fontname="verdana.ttf",
                  fontsize=10,
                  bold=False,
                  italic=False):
         super(Button, self).__init__()
+#test
